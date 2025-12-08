df3b92f39e11eb43a3e45f54833b6f8bd0dd1f11
saiteja-yernagula <saiteja.yernagula@gmail.com>
2025-12-08 17:05:43 +0530

    'smg'

---
+++ day42/demo.js
@@ -0,0 +1,13 @@
+export default function demo(){
+    console.log("default func-This is a demo function from day42/demo.js");
+}
+
+function fun1(){
+    console.log("named func-This is a named function from day42/demo.js");
+}
+function fun2(){
+    console.log("named func-This is another named function from day42/demo.js");
+}
+
+export {fun1,fun2};
+
